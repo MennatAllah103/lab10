6a8db22c94e41a081b2a97125c4d2a75b68ef683
MennatAllah103 <mennagenaidy150@gmail.com>
2024-12-12 01:07:55 +0200

    modified group management class

--- src/Backend/GroupManagement.java
+++ src/Backend/GroupManagement.java
@@ -13,35 +13,37 @@ import java.util.ArrayList;
  */
 public class GroupManagement {
     GroupDataBase database=GroupDataBase.getInstance();
-      
-  
+    GroupRequestDatabase requestDatabase = GroupRequestDatabase.getinstance(); 
+    UserDataBase userData=UserDataBase.getDatabase();
+     ArrayList<GroupRequests>requests=requestDatabase.getALLRequests();
+     
     //Admin
-   public boolean addMember(User member,String groupName) 
+   public void addMember(User member,String groupName) 
    {  User currentuser = UserDataBase.getCurrentUser();
     Group group=database.getGroupByname(groupName);
-      if (group == null) {
-        System.out.println("Group not found");
-        return false;
-    }
+      // (group == null) {
+        //System.out.println("Group not found");
+       // return false;
+    //}
     ArrayList<User> members=group.getMembers();
-    if(members.contains(member))   
-       return false;
-    else{
+   // if(members.contains(member))   
+       //return false;
+    //else{
         members.add(member);
          GroupDataBase.saveGroupToFile(GroupDataBase.getGroups());
-        return true;
-    }
+        //return true;
+    //}
    }
-    public boolean removeMember(User member,String groupName)
+    public void removeMember(User member,String groupName)
     {  User currentuser = UserDataBase.getCurrentUser();
      Group group=database.getGroupByname(groupName);
-       if (group == null) {
-        System.out.println("Group not found");
-        return false;
-    }
+     /*if (group == null) {
+     System.out.println("Group not found");
+     return false;
+     }*/
     ArrayList<User> members=group.getMembers();
     ArrayList<User> admins=group.getAdmins();
-    if(!members.contains(member))
+  /*  if(!members.contains(member))
        return false;
     else if(!admins.contains(currentuser))
     {  System.out.println("current user not admin , can not edit memebers");
@@ -51,64 +53,106 @@ else if(member.getUserId().equals(group.getPrimaryAdminId())){
          return false;
    
 }
-       else 
+       else */
         members.remove(member);
          GroupDataBase.saveGroupToFile(GroupDataBase.getGroups());
-        return true;
+       // return true;
     
-   }  
+   } 
+    
+    
+     public void acceptrequest(GroupRequests request)
+    
+     {
+       String memberId= request.getMemberId();
+       String groupId=request.getGroupId();
+       User newMember=userData.getUserById(memberId);
+       Group group=database.getGroupById(groupId);
+       database.addGroup(group);
+      ArrayList<User> members= group.getMembers();
+      members.add(newMember);
+       requests.remove(request);
+       requestDatabase.saveFile(requests);
+      
+    
+    
+    
+     }
+    public void Declinerequest(GroupRequests request)
+    
+     {
+       
+       deleterequest(request);
+    
+    
+    
+     }
+    
+      public void deleterequest(GroupRequests request)
+    {
+        requests.remove(request);
+     
+       requestDatabase.saveFile(requests);
+    }
+    
+    
+    
+    
+    
+    
+    
 
     
     
     //PrimaryAdmin
-public boolean promoteToAdmin(User member,String groupName)
+public void promoteToAdmin(User member,String groupName)
 {    User currentuser = UserDataBase.getCurrentUser();
     Group group=database.getGroupByname(groupName);
-      if (group == null) {
+     /* if (group == null) {
         System.out.println("Group not found");
         return false;
-    }
+    }*/
     ArrayList<User> members=group.getMembers();
     ArrayList<User> admins=group.getAdmins();
     
-    if(members.contains(member) && !admins.contains(member) && currentuser.getUserId().equals(group.getAdminId()))
+   /* if(members.contains(member) && !admins.contains(member) && currentuser.getUserId().equals(group.getAdminId()))
     {admins.add(member);
      GroupDataBase.saveGroupToFile(GroupDataBase.getGroups());
     return true;}
     
-    else return false;
+    else return false;*/
     
 } 
 
-public boolean DemoteFromAdmin(User admin,String groupName)
+public void DemoteFromAdmin(User admin,String groupName)
 {   User currentuser = UserDataBase.getCurrentUser();
     Group group=database.getGroupByname(groupName);
-      if (group == null) {
+      /*if (group == null) {
         System.out.println("Group not found");
         return false;
-    }
+    }*/
     ArrayList<User> admins=group.getAdmins();
     
-    if(admins.contains(admin)&& currentuser.getUserId().equals(group.getPrimaryAdminId()))
+    /*if(admins.contains(admin)&& currentuser.getUserId().equals(group.getPrimaryAdminId()))
     {admins.remove(admin);
      GroupDataBase.saveGroupToFile(GroupDataBase.getGroups());
     return true;}
     
     else{ System.out.println("Current user is not primary admin,can't demote admin");
-        return false;}
+        return false;}*/
     
 } 
  public void deleteGroup(String groupName) {
      User currentuser = UserDataBase.getCurrentUser();
         Group group = database.getGroupByname(groupName);
-        if (group ==null ) {
+        /*if (group ==null ) {
           System.out.println("Group is null");
         }
         else if(!currentuser.getUserId().equals(group.getPrimaryAdminId()))
             System.out.println("Current user not primary admin,can't delete group");
-        else{
+        else{*/
             GroupDataBase.getGroups().remove(group);
             GroupDataBase.saveGroupToFile(GroupDataBase.getGroups());
-    }  
+    //}  
  }
 }
